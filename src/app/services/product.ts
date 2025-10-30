1ea207d7572255c668eb6792223a1f169811be8c
Andre-Franco1 <andrefranco3172@gmail.com>
2025-10-30 13:30:22 -0300

    fix: remove hardcoded product array and changed saveProduct method logic

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -8,11 +8,6 @@ import { CategoryService } from './category';
 export class ProductService {
   
   products : Product [] = [];
-  // products : Product [] = [
-  //     {id : 1, name : 'Product 1', description: 'Description of a product (1)', price: 50.00, category: , promotion: true, newProduct: false},
-  //     {id : 2, name : 'Product 2', description: 'Description of a product (2)', price: 75.50, category: , promotion: false, newProduct: false},
-  //     {id : 3, name : 'Product 3', description: 'Description of a product (3)', price: 32.95, category: , promotion: true, newProduct: true}
-  //   ]
 
   getProducts() : Product[] {
       return this.products;
